785e8e044ad3562938077c56372afef53c8ca041
endi julian <endi@iglobal.co.id>
2021-11-21 04:02:13 +0700

    finish slicing

---
+++ lib/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+Color blueColor = Color(0XFF1A73E8);
+Color whiteColor = Color(0XFFFFFFFF);
+Color lightBlueColor = Color(0XFF9ED0FF);
+Color blackColor = Color(0XFF2C3A59);
+Color greyColor = Color(0XFF808BA2);
+Color greenColor = Color(0XFF29CB9E);
+
+TextStyle titleTextStyle = TextStyle(
+  color: blackColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 16,
+);
+
+TextStyle subtitleTextStyle = TextStyle(
+  color: greyColor,
+  fontSize: 14, //mau dipake apa engga, karena default fontsize nya flutter 14px
+  fontWeight: FontWeight.w300,
+);
---
+++ lib/widgets/chat_tile.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chatty/theme.dart';
+
+class ChatTile extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+  final String text;
+  final String time;
+  final bool unread;
+
+  ChatTile({
+    required this.imageUrl,
+    required this.name,
+    required this.text,
+    required this.time,
+    required this.unread,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 16),
+      child: Row(
+        children: [
+          Image.asset(
+            // 'assets/images/friend1.png',
+            imageUrl,
+            height: 55,
+            width: 55,
+          ),
+          SizedBox(
+            width: 12,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                // 'Joshuer',
+                name,
+                style: titleTextStyle,
+              ),
+              Text(
+                // 'Sorry, you’re not my ty...',
+                text,
+                style: unread
+                    ? subtitleTextStyle.copyWith(color: blackColor)
+                    : subtitleTextStyle,
+              )
+            ],
+          ),
+          Spacer(),
+          Text(
+            // 'Now',
+            time,
+            style: subtitleTextStyle,
+          )
+        ],
+      ),
+    );
+  }
+}
